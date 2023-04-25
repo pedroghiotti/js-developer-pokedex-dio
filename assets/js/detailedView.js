3a87c1e44dc39fa098b049fe146e7eb35b42b665
Pedro Ghiotti Martins <pedro.ghiotti.m@gmail.com>
2023-04-25 20:31:45 -0300

    Página de detalhes finalizada.
    Funcionalidade e design da página de detalhes finalizada. Se for haver mais updates, vão ser à página principal.

--- assets/js/detailedView.js
+++ assets/js/detailedView.js
@@ -1,66 +1,76 @@
-const detailedView = 
-{
-    element: document.querySelector('#detailView'),
-    pokemonList: document.querySelector('#detailViewPokemonList'),
-    idList: document.querySelector('#detailViewIdList')
-}
+const detailedView = {
+  element: document.querySelector("#detailView"),
+  pokemonList: document.querySelector("#detailViewPokemonList"),
+  idList: document.querySelector("#detailViewIdList"),
+};
 
 let currPokemonId = 1;
 
 // Function Declaractions //
 detailedView.ScrollToId = (id) => {
-    currPokemonId = id;
+  currPokemonId = id;
 
-    if(currPokemonId >= offset + limit - 1) loadFurtherPokemon();
+  if (currPokemonId >= offset + limit - 1) loadFurtherPokemon();
 
-    const pageElementWidth = detailedView.pokemonList.getBoundingClientRect().width; // Largura do elemento de detalhes.
-    const idElementWidth = document.querySelector('.listedId').getBoundingClientRect().width; // Largura do elemento de ID.
-    
-    // Movendo scroll para posição.
-    detailedView.pokemonList.scrollTo(pageElementWidth * (id - 1), 0);
-    /*
+  const pageElementWidth =
+    detailedView.pokemonList.getBoundingClientRect().width; // Largura do elemento de detalhes.
+  const idElementWidth = document
+    .querySelector(".listedId")
+    .getBoundingClientRect().width; // Largura do elemento de ID.
+
+  // Movendo scroll para posição.
+  detailedView.pokemonList.scrollTo(pageElementWidth * (id - 1), 0);
+  /*
         Aqui eu adiciono um offset ao id pois tenho elementos adicionais para permitir
         o efeito da lista com o elemento atual centralizado.
     */
-    detailedView.idList.scrollTo(idElementWidth * (id + 4), 0);
-}
+  detailedView.idList.scrollTo(idElementWidth * (id + 4), 0);
+};
 detailedView.ScrollToNext = () => {
-    if(currPokemonId >= maxRecords) return;
-    detailedView.ScrollToId( currPokemonId + 1 );
-}
+  if (currPokemonId >= maxRecords) return;
+  detailedView.ScrollToId(currPokemonId + 1);
+};
 detailedView.ScrollToPrev = () => {
-    if(currPokemonId <= 1) return;
-    detailedView.ScrollToId( currPokemonId - 1 );
-}
+  if (currPokemonId <= 1) return;
+  detailedView.ScrollToId(currPokemonId - 1);
+};
 detailedView.OpenOnId = (id) => {
-    detailedView.ScrollToId(id);
-    detailedView.element.classList.remove('hidden');
-}
+  detailedView.ScrollToId(id);
+  detailedView.element.classList.remove("hidden");
+};
 
 // Event Bindings //
-document.getElementById('detailViewIdPrev').addEventListener('click', () => {
-    detailedView.ScrollToPrev();
+document.getElementById("detailViewIdPrev").addEventListener("click", () => {
+  detailedView.ScrollToPrev();
 });
-document.getElementById('detailViewIdNext').addEventListener('click', () => {
-    detailedView.ScrollToNext();
+document.getElementById("detailViewIdNext").addEventListener("click", () => {
+  detailedView.ScrollToNext();
 });
-document.getElementById('detailViewReturnButton').addEventListener('click', () => {
-    detailedView.element.classList.add('hidden');
-});
+document
+  .getElementById("detailViewReturnButton")
+  .addEventListener("click", () => {
+    detailedView.element.classList.add("hidden");
+  });
 
 // Atualizo a posição do scroll se a janela for redimensionada.
-window.addEventListener('resize', function(event) {
+window.addEventListener(
+  "resize",
+  function (event) {
     detailedView.ScrollToId(currPokemonId);
-}, true);
+  },
+  true
+);
 
 // Initialization //
 (() => {
-    let idListStr = '';
+  let idListStr = "";
 
-    for(let i = 1; i <= maxRecords; i++)
-    {
-        idListStr += `<span class="listedId">${i}</span>`
-    }
+  for (let i = 1; i <= maxRecords; i++) {
+    idListStr += `<span class="listedId">${i}</span>`;
+  }
 
-    detailedView.idList.innerHTML = detailedView.idList.innerHTML.replace('!', idListStr);
+  detailedView.idList.innerHTML = detailedView.idList.innerHTML.replace(
+    "!",
+    idListStr
+  );
 })();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,143 @@
-const pokemonList = document.getElementById('pokemonList');
-const loadMoreButton = document.getElementById('loadMoreButton');
-const detailView = document.getElementById('detailView');
-const detailViewReturnButton = document.getElementById('detailViewReturnButton');
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const detailView = document.getElementById("detailView");
+const detailViewReturnButton = document.getElementById(
+  "detailViewReturnButton"
+);
 
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
+  return `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
                 </ol>
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `;
 }
 function convertPokemonToDetailedLi(pokemon) {
-    return `
-        <div class="listedDetails ${pokemon.type}">
-            <span class="name">${pokemon.name}</span>
-            <img src="${pokemon.photo}" alt="">
+  return `
+    <div class="listedDetails ${pokemon.type}">
+    <span class="name">${pokemon.name}</span>
+    <img src="${pokemon.photo}" alt="">
+    <div class = "detailBlock">
+        <div class="typesBlock">
+            ${pokemon.types
+              .map(
+                (type) => `<div class="typeDetailedView ${type}">
+            <div class="typeIcon ${type}Icon"></div>
+            <span class="typeText">${type}</span>
+        </div>`
+              )
+              .join("")}
         </div>
-    `
+        <div class="statsBlock">
+            <div class = "stat">
+                <span class = "statName">HP</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[0].base_stat / 255) * 100
+                    }%; background-color: #ff0000;"></div>
+                </span>
+            </div>
+            <div class = "stat">
+                <span class = "statName">Def</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[1].base_stat / 255) * 100
+                    }%; background-color: #F08030;"></div>
+                </span>
+            </div>
+            <div class = "stat">
+                <span class = "statName">Atk</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[2].base_stat / 255) * 100
+                    }%; background-color: #F8D030;"></div>
+                </span>
+            </div>
+            <div class = "stat">
+                <span class = "statName">HP</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[3].base_stat / 255) * 100
+                    }%; background-color: #F85888;"></div>
+                </span>
+            </div>
+            <div class = "stat">
+                <span class = "statName">HP</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[4].base_stat / 255) * 100
+                    }%; background-color: #78C850;"></div>
+                </span>
+            </div>
+            <div class = "stat">
+                <span class = "statName">HP</span>
+                <span class = "statBar">
+                    <div class="statSlider" style="width: ${
+                      (pokemon.stats[5].base_stat / 255) * 100
+                    }%; background-color: #F85888;"></div>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+    `;
 }
-function UpdatePokemonEventListeners()
-{
-    const addedPokemon = document.querySelectorAll('.pokemon');
-    for(let i = 0; i < addedPokemon.length; i++)
-    {
-        addedPokemon[i].addEventListener('click', () => OpenDetailedView(addedPokemon[i]));
-    }
+function UpdatePokemonEventListeners() {
+  const addedPokemon = document.querySelectorAll(".pokemon");
+  for (let i = 0; i < addedPokemon.length; i++) {
+    addedPokemon[i].addEventListener("click", () =>
+      OpenDetailedView(addedPokemon[i])
+    );
+  }
 }
-function OpenDetailedView(pokemon)
-{
-    detailedView.OpenOnId(Number(pokemon.querySelector('.number').innerText.replace('#', '')));
+function OpenDetailedView(pokemon) {
+  detailedView.OpenOnId(
+    Number(pokemon.querySelector(".number").innerText.replace("#", ""))
+  );
 }
 
 function loadPokemonItems(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newPokemonListHtml = pokemons.map(convertPokemonToLi).join('');
-        const newPokemonDetailedHtml = pokemons.map(convertPokemonToDetailedLi).join('');
-        pokemonList.innerHTML += newPokemonListHtml;
-        detailedView.pokemonList.innerHTML += newPokemonDetailedHtml;
-        UpdatePokemonEventListeners();
-    });
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newPokemonListHtml = pokemons.map(convertPokemonToLi).join("");
+    const newPokemonDetailedHtml = pokemons
+      .map(convertPokemonToDetailedLi)
+      .join("");
+    pokemonList.innerHTML += newPokemonListHtml;
+    detailedView.pokemonList.innerHTML += newPokemonDetailedHtml;
+    UpdatePokemonEventListeners();
+  });
 }
 
-function loadFurtherPokemon()
-{
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+function loadFurtherPokemon() {
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItems(offset, newLimit)
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItems(offset, newLimit);
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItems(offset, limit)
-    }
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItems(offset, limit);
+  }
 }
 
-loadMoreButton.addEventListener('click', loadFurtherPokemon);
+loadMoreButton.addEventListener("click", loadFurtherPokemon);
 
 loadPokemonItems(offset, limit);
